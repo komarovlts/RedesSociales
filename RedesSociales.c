5b3dff0bcabb0b83fdf5ddde9be1889080745481
ocarrascoe <omar.carrasco@usach.cl>
2017-10-19 11:38:02 -0300

    Creación de Matriz y Texto de Entrada
    Se declaró de manera global una matriz de 5x5 y se crearon dos
    funciones, una rellena la matriz con 0s y la otra le imprime.

--- RedesSociales.c
+++ RedesSociales.c
@@ -1,2 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int vertices = 5;
+int matriz[5][5];
+
+void crearMatrizVacia(){ // Matriz rellena con ceros (0).
+   int i, j;
+   for (i = 0; i < vertices; i++){
+      for (j = 0; j < vertices; j++){
+         matriz[i][j] = 0;
+      }
+   }
+   return;
+}
+
+void mostrarMatriz(){
+   int i,j;
+   for(i = 0;i < vertices ; i++){
+      for(j = 0 ; j < vertices; j++){
+         printf("[ %d ]", matriz[i][j] );
+      }
+   printf("\n");
+   }
+   return;
+}
+
+int main(int argc, char const *argv[]) {
+   crearMatrizVacia();
+   mostrarMatriz();
+   return 0;
+}
