646c1ec7eb52247ef72c1a0915a9e3f7cebffa5b
ocarrascoe <omar.carrasco@usach.cl>
2017-10-20 20:49:17 -0300

    Se deja funcional esta función.
    Se quitó código basura y ahora imprime correctamente cada número del
    archivo de forma lineal, se añadieron comentarios para entendimiento del
    código además de variables con nombres adecuados.

--- fileUse.c
+++ fileUse.c
@@ -2,47 +2,36 @@
 
 int main(){
 
-   int n,i,j,k,l,ady;
-	char c;
-	FILE *fp=fopen("Entrada.in","r");
+   int cantidadVertices,vertice;
+	char espacio;
+	FILE *archivoObjetivo=fopen("Entrada.in","r");
 
-	//OBTIENE LA CANTIDAD DE VERTICES//
-	fscanf(fp,"%d",&n);
-	if(n < 0){
+	//Obtener la cantidad de vertices.
+	fscanf(archivoObjetivo,"%d",&cantidadVertices);
+   //Para números negativos o algo similar al inicio del texto.
+	if(cantidadVertices < 0){
 		printf("ERROR DE FORMATO EN EL ARCHIVO DE TEXTO (CANTIDAD DE VERTICES)\n");
 		return 0;
 	}
-	if(n == 0){
+   //Si no existen vertices, grafo nulo.
+	if(cantidadVertices == 0){
 		printf("EL GRAFO NO POSEE VERTICES (GRAFO NULO)\n");
 		return 0;
 	}
-   printf("Número de vertices: %d\n", n);
-   for (l = 1; l < n+1 ; l++)	{
-		do{
-			fscanf(fp,"%d%c",&ady,&c);
-			if (ady>n||ady<0){
+   //Imprimir la cantidad de vertices (primer número del archivo).
+   printf("Número de vertices: %d\n", cantidadVertices);
+   //Recorrer el archivo imprimiendo cada uno de los números hasta el final del archivo.
+   while(!feof(archivoObjetivo)){
+			fscanf(archivoObjetivo,"%d%c",&vertice,&espacio);
+			if(vertice > cantidadVertices  ||  vertice < 0){
 				printf("ERROR DE FORMATO EN EL ARCHIVO DE TEXTO (VERTICES ADYACENTES)\n" );
 				return 1;
 			}
-			if(ady!=0){
-				printf("Prueba: %d,%d\n", l,ady);
+			if(vertice != 0  &&  !feof(archivoObjetivo)){
+				printf("Vertice: %d\n",vertice);
 			}
-		}while(c ==' '&& !feof(fp));
 	}
-   fclose(fp);
+   //Cerrar el archivo -> buenas prácticas y evita errores.
+   fclose(archivoObjetivo);
    return 0;
-   /*
-   char buff[255];
-
-   fscanf(fp, "%s", buff);
-   printf("1 : %s\n", buff );
-
-   fgets(buff, 255, (FILE*)fp);
-   printf("2: %s\n", buff );
-
-   fgets(buff, 255, (FILE*)fp);
-   printf("3: %s\n", buff );
-   fclose(fp);*/
-
-
 }
