fd061ef1d483c1bd9425eebcb8ae4d258f5fecf0
ocarrascoe <omar.carrasco@usach.cl>
2017-10-21 19:52:50 -0300

    Listas de Adyacencias
    Se agregó todo lo vinculado a lista de adyacencia, 3 structs en total y
    sus respectivas funciones de inicialización, adherencia y de mostrar
    según corresponda.

--- RedesSociales.c
+++ RedesSociales.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void crearMatrizVacia(int cantidadVertices, int matrizAdyacencia[cantidadVertices][cantidadVertices]){ // Matriz rellena con ceros (0).
+//Declaración de estructuras para la lista de adyacencia.
+typedef struct verticeAdyacente{
+   int verticeConectado;
+   struct verticeAdyacente *siguiente;
+}VA;
+
+typedef struct Vertice{
+   int numeroVertice;
+   int longitud;
+   VA *inicio;
+   struct Vertice *siguiente;
+}Vertice;
+
+typedef struct listaAdyacencia{
+   Vertice *inicio;
+}LA;
+
+//Declaración de variables globables.
+LA listaAdyacencia;
+int longitudLA;
+int longitudVA;
+
+void inicializarListaAdyacencia(){
+   listaAdyacencia.inicio = NULL;
+   longitudLA = 0;
+   return;
+}
+
+void vaciarMatriz(int cantidadVertices, int matrizAdyacencia[cantidadVertices][cantidadVertices]){ // Matriz rellena con ceros (0).
    int i, j;
    for (i = 0; i < cantidadVertices; i++){
       for (j = 0; j < cantidadVertices; j++){
@@ -20,6 +48,113 @@ void mostrarMatriz(int cantidadVertices, int matrizAdyacencia[cantidadVertices][
    printf("\n");
    }
    return;
+}
+
+Vertice* crearVertice(int numeroVertice){
+   Vertice* vertice = (Vertice*)malloc(sizeof(Vertice));
+   vertice->numeroVertice = numeroVertice;
+   vertice->longitud = 0;
+   vertice->inicio = NULL;
+   vertice->siguiente = NULL;
+   return vertice;
+}
+
+VA* crearVerticeAdyacente(int verticeConectado){
+   VA* verticeAdyacente = (VA*)malloc(sizeof(VA));
+   verticeAdyacente->verticeConectado = verticeConectado;
+   verticeAdyacente->siguiente = NULL;
+   return verticeAdyacente;
+}
+
+void agregarVertice(Vertice* vertice){
+   int i;
+	if(listaAdyacencia.inicio == NULL){
+		listaAdyacencia.inicio = vertice;
+		longitudLA = 1;
+		return;
+	}
+	else{
+		Vertice * verticeAux = listaAdyacencia.inicio;
+		for (i = 0; i < longitudLA-1; i++){
+			verticeAux = verticeAux -> siguiente;
+		}
+		verticeAux -> siguiente = vertice;
+		longitudLA++;
+		return;
+		}
+}
+
+void agregarVerticeAdyacente(Vertice* verticeObjetivo, VA* verticeAdyacente){
+   int i;
+   Vertice* verticeAux = verticeObjetivo;
+	if(verticeAux->inicio == NULL){
+		verticeAux->inicio = verticeAdyacente;
+		verticeAux->longitud = 1;
+		return;
+	}
+	else{
+		VA* verticeAdyacenteAux = verticeAux->inicio;
+		for (i = 0; i < verticeAux->longitud-1; i++){
+			verticeAdyacenteAux = verticeAdyacenteAux -> siguiente;
+		}
+		verticeAdyacenteAux -> siguiente = verticeAdyacente;
+		verticeAux->longitud++;
+		return;
+		}
+}
+
+void crearListaAdyacencia(int cantidadVertices, int matrizAdyacencia[cantidadVertices][cantidadVertices]){
+   int i;
+   for (i = 0; i < cantidadVertices; i++){
+      Vertice* vertice = crearVertice(i+1);
+      agregarVertice(vertice);
+   }
+}
+
+void rellenarListaAdyacencia(int cantidadVertices, int matrizAdyacencia[cantidadVertices][cantidadVertices]){
+   int i,j;
+   for (i = 0; i < cantidadVertices; i++){
+      for (j = 0; j < cantidadVertices; j++){
+         if (matrizAdyacencia[i][j] == 1) {
+            Vertice * verticeAux = listaAdyacencia.inicio;
+         	while(verticeAux->numeroVertice != i+1   ){
+         		verticeAux = verticeAux -> siguiente;
+         	}
+            VA* verticeAdyacente = crearVerticeAdyacente(j);
+            verticeAdyacente -> verticeConectado = j+1;
+            agregarVerticeAdyacente(verticeAux,verticeAdyacente);
+         }
+      }
+   }
+   return;
+}
+
+void mostrarVerticeAdyacente(VA* verticeAdyacente) {
+   printf("  %d  ", verticeAdyacente -> verticeConectado);
+   return;
+}
+
+void mostrarVertice(Vertice* vertice){
+	printf("Vertice: %d ->", vertice -> numeroVertice);
+   VA* verticeAdyacenteAux = vertice->inicio;
+	while(verticeAdyacenteAux-> siguiente != NULL){
+		mostrarVerticeAdyacente(verticeAdyacenteAux);
+		verticeAdyacenteAux = verticeAdyacenteAux -> siguiente;
+	}
+	mostrarVerticeAdyacente(verticeAdyacenteAux);
+	printf("\n");
+   return;
+}
+
+void mostrarListaAdyacencia(){ // Sólo para uso del Desarrollador.
+   printf("\n");
+   Vertice* verticeAux = listaAdyacencia.inicio;
+	while(verticeAux -> siguiente != NULL){
+		mostrarVertice(verticeAux);
+		verticeAux = verticeAux -> siguiente;
+	}
+	mostrarVertice(verticeAux);
+	printf("\n");
 }
 
 int main(int argc, char const *argv[]) {
@@ -41,7 +176,7 @@ int main(int argc, char const *argv[]) {
    //Imprimir la cantidad de vertices (primer número del archivo).
    printf("Número de vertices: %d\n", cantidadVertices);
    //Creación de la matriz vacía.
-   crearMatrizVacia(cantidadVertices,matrizAdyacencia);
+   vaciarMatriz(cantidadVertices,matrizAdyacencia);
    //Imprimir la matriz vacía.
    printf("\nLa matriz de adyacencia vacía es: \n");
    mostrarMatriz(cantidadVertices,matrizAdyacencia);
@@ -64,5 +199,14 @@ int main(int argc, char const *argv[]) {
    mostrarMatriz(cantidadVertices,matrizAdyacencia);
    //Cerrar el archivo -> buenas prácticas y evita errores.
    fclose(archivoObjetivo);
+   //Fin de lectura del archivo.
+   //Creación de lista de adyacencia.
+   inicializarListaAdyacencia();
+   crearListaAdyacencia(cantidadVertices,matrizAdyacencia);
+   rellenarListaAdyacencia(cantidadVertices,matrizAdyacencia);
+   mostrarListaAdyacencia();
+
+
+
    return 0;
 }
